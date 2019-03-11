4fefb18646f6243be0afc8e6637422a847da12e1
Roman Nazarov <nazarovbusiness@gmail.com>
2019-03-11 13:33:47 +0700

    adde dfolder exist? method to folders lib

--- lib/control/folder.rb
+++ lib/control/folder.rb
@@ -8,12 +8,16 @@ module Control
       @folder = folder
     end
 
+    def folder_exist?
+      File.directory? @folder.path
+    end
+
     def create
-      FileUtils.mkdir_p @folder.path
+      FileUtils.mkdir_p(@folder.path)
     end
 
     def delete
-      FileUtils.rm_rf @folder.path
+      FileUtils.rm_rf(@folder.path) if folder_exist?
     end
   end
 end
